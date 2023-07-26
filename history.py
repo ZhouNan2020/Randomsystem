2cb26c25d8c1e058f483aa19eeb2818d7272b05b
ZhouNan <zhounan@1utar.my>
2023-07-26 17:34:55 +0800

    Update history.py

--- history.py
+++ history.py
@@ -30,7 +30,7 @@ df.columns = ["随机时间", "中心名称", "患者唯一识别码","随机号
 st.dataframe(df)
 # 提供下载st.download_button,使用户可以下载df到本地任意路径，文件名为"随机表.csv"
 
-@st.cache
+
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
@@ -42,4 +42,4 @@ st.download_button(
     data=csv,
     file_name='随机表.csv',
     mime='text/csv',
-)
+    )
