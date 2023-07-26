687fb8fa422cd2095e5dc85d90f8b99c6b6e8caa
ZhouNan <zhounan@1utar.my>
2023-07-26 15:43:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,64 +23,91 @@ def connect_to_google_sheet():
         return sheet_A,sheet_B
 
 
-st.header("随机系统")
-risk_level = st.radio("请选择风险分层", ("高风险", "中风险"))
-sheet_A, sheet_B = connect_to_google_sheet()
-first_column = sheet_B.col_values(1)
-available_numbers = set(range(1, 501)) - set(map(int, first_column))
-# 提供一个单选下拉，让用户选择研究中心
-center = st.selectbox("请选择研究中心", ['广西医科大学第一附属医院',	
-                                        '河池市第一人民医院',	
-                                        '百色市人民医院',	
-                                        '平果市人民医院',	
-                                        '来宾市人民医院',
-                                        '武鸣区中医医院',	
-                                        '宾阳县人民医院',	
-                                        '广安市人民医院',	
-                                        '阆中市人民医院',
-                                        '攀枝花市中心医院',	
-                                        '通江县人民医院',	
-                                        '达州市达川区人民医院',	
-                                        '宣汉县人民医院',	
-                                        '凉山州第一人民医院',
-                                        '巴中市中心医院',	
-                                        '南江县人民医院'])
-patient_id = st.text_input("请输入患者的住院号")
 
-group = np.nan
-random_number = np.nan
-if st.button("开始随机"):
-    if risk_level == "高风险":
-         # 首先从1-500中排除first_column中已经存在的数字，然后在剩下的数字中随机抽取一个
-         random_number = random.choice(list(available_numbers))
-         group = np.nan
-         if random_number % 2 == 0:
-             group = "对照组"
-         else:
-             group = "试验组"
-         random_number = "ONDEX" + str(random_number)
-    elif risk_level == "中风险":
-        random_number = random.choice(list(available_numbers))
+# Define a function to verify the user's credentials
+def verify_credentials(username, password):
+    # Here you should implement the logic to verify the user's credentials
+    # For simplicity, we will just check if the username is "admin" and the password is "password"
+    if username in ["test",'test1'] and password in ["000",'111']:
+        return True
+    else:
+        return False
+
+# Create a form for the user to enter their credentials
+with st.form(key='login_form'):
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    submit_button = st.form_submit_button(label='Login')
+if submit_button:
+    if verify_credentials(username, password):
+        st.success("Login successful")
+        # Display a data interface after successful login
+        st.header("随机系统")
+        risk_level = st.radio("请选择风险分层", ("高风险", "中风险"), key='risk_level')
+        sheet_A, sheet_B = connect_to_google_sheet()
+        first_column = sheet_B.col_values(1)
+        available_numbers = set(range(1, 501)) - set(map(int, first_column))
+        # 提供一个单选下拉，让用户选择研究中心
+        center = st.selectbox("请选择研究中心", ['广西医科大学第一附属医院',	
+                                                '河池市第一人民医院',	
+                                                '百色市人民医院',	
+                                                '平果市人民医院',	
+                                                '来宾市人民医院',
+                                                '武鸣区中医医院',	
+                                                '宾阳县人民医院',	
+                                                '广安市人民医院',	
+                                                '阆中市人民医院',
+                                                '攀枝花市中心医院',	
+                                                '通江县人民医院',	
+                                                '达州市达川区人民医院',	
+                                                '宣汉县人民医院',	
+                                                '凉山州第一人民医院',
+                                                '巴中市中心医院',	
+                                                '南江县人民医院'], key='center')
+        patient_id = st.text_input("请输入患者的住院号", key='patient_id')
+        
         group = np.nan
-        if random_number % 2 == 0:
-            group = "对照组"
-        else:
-            group = "试验组"
-        random_number = "ONDEX" + str(random_number)
-random_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))).isoformat()
-if st.button("确认随机结果"):
+        random_number = np.nan
+        if st.button("开始随机"):
+            if risk_level == "高风险":
+                 # 首先从1-500中排除first_column中已经存在的数字，然后在剩下的数字中随机抽取一个
+                 random_number = random.choice(list(available_numbers))
+                 group = np.nan
+                 if random_number % 2 == 0:
+                     group = "对照组"
+                 else:
+                     group = "试验组"
+                 random_number = "ONDEX" + str(random_number)
 
-    sheet_A.append_row([random_time,center,patient_id, random_number,risk_level,group],1)       
+            elif risk_level == "中风险":
+                random_number = random.choice(list(available_numbers))
+                group = np.nan
+                if random_number % 2 == 0:
+                    group = "对照组"
+                else:
+                    group = "试验组"
+                random_number = "ONDEX" + str(random_number)
+        random_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))).isoformat()
+
+
+        if st.button("确认随机结果"):
+        
+            sheet_A.append_row([random_time,center,patient_id, random_number,risk_level,group],1)       
+                
+                
+                 
+                 
+
+                 
+
+             
+
+        
+
+
+        # Display a dataframe with example data
         
         
-         
-        
-        
-
-
-
-
-
-
-
+    else:
+        st.error("Invalid username or password")
 
